2ff181f2adcb75824923734ac0b5196e159b37d9
hakkirinihongo <andreastevoli@libero.it>
2026-02-26 12:19:34 +0100

    Disable sitemap integration (temporary)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hakkirinihongo.com',
-  integrations: [
-    mdx(),
-    sitemap()
-  ],
+  integrations: [mdx()],
 });
