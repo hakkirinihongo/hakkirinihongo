f9ecb4bd05b8cc181c84f5305f721643582aaebe
hakkirinihongo <andreastevoli@libero.it>
2026-03-12 22:43:14 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,4 @@ export default defineConfig({
   site: 'https://hakkirinihongo.com',
   integrations: [mdx()],
 });
-// astro.config.mjs
-integrations: [
-  sitemap({
-    serialize(item) {
-      if (item.url.endsWith('/') && item.url.length > (new URL(item.url).origin.length + 1)) {
-        item.url = item.url.slice(0, -1);
-      }
-      return item;
-    },
-  }),
-],
-  export default defineConfig({
-  trailingSlash: 'never',
-  build: {
-    format: 'file', // Fondamentale per generare 'about.html' invece di 'about/'
-  },
-});
 
