0eb1065c8a48062313168e69017e5716e8226fa4
hakkirinihongo <andreastevoli@libero.it>
2026-03-15 18:23:48 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hakkirinihongo.com',
   trailingSlash: 'always',
   integrations: [
-    mdx(),
-    sitemap()
+    mdx()
   ],
 });
-
